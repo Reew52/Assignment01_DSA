6031eb6d6e170058bc8ffdf06759f622282b85d9
Reew52 <koydzxxx@gmail.com>
2023-02-28 22:21:34 +0700

    update PowerFunction

--- src/PowerFunction_P2/PowerFunction.java
+++ src/PowerFunction_P2/PowerFunction.java
@@ -3,7 +3,7 @@ package PowerFunction_P2;
 public class PowerFunction {
     public static void main(String[] args) {
         int base = 2;
-        int exponent = 5;
+        int exponent = 3;
         int result = power(base, exponent);
         System.out.println(base + " raised to the power of " + exponent + " is " + result);
     }
